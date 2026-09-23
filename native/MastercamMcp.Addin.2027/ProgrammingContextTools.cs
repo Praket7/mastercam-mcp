4f930e1ee5dfcc60d40095f58c6eea475e20bf75
Praket7 <149203433+Praket7@users.noreply.github.com>
2026-09-22 19:55:19 -0500

    Make Stage-B source portable-test compilable

--- native/MastercamMcp.Addin.2027/ProgrammingContextTools.cs
+++ native/MastercamMcp.Addin.2027/ProgrammingContextTools.cs
@@ -1,4 +1,8 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Text.Json;
+using System.Threading;
 using MastercamMcp.Adapter.Abstractions;
 using MastercamMcp.ReadModel;
 
